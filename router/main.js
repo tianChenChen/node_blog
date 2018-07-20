591f9eb48f9345f72e4ed3da11e85c163c493377
celia <tcc_2007@126.com>
2018-07-20 11:16:37 +0800

    内容

--- router/main.js
+++ router/main.js
@@ -3,16 +3,36 @@ var router = express.Router();
 
 var Category =  require('../models/Category')
 
+// 首页
 router.get('/', function(req, res, next){
+
+  var data = {
+    userInfo: req.userInfo,
+    categories: [],
+    count: 0,
+    page: Number(req.query.page || 1),
+    limit: 10,
+    pages: 0
+  }
+  
+
   // 读取所有的分类信息
   Category.find().then(function(categories){
-    console.log(categories)
 
-    res.render('main/index', {
-      userInfo: req.userInfo,
-      categories: categories
-    })
+    data.categories = categories
+    return Content.count()
 
+  }).then(function(count){
+    data.count = count
+    data.pages = Math.ceil(data.count/ data.limit);
+    data.page = Math.min(data.page, data.pages);
+    data.page = Math.max(data.page, 1);
+    var skip = (data.page - 1) * data.limit;
+
+    return Content.find().limit(limit).skip(skip).populate(['category', 'user'])
+  }).then(function(contents){
+    data.contents = contents
+    res.render('main/index', data)
   })
 })
 
